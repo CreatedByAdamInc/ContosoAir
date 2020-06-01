f54098b729ea424c46c1702b4b4878ed3a997e6c
Adam <adamthecreator@github.com>
2020-06-01 09:29:25 -0700

    Changes airport sorting. Fixes AB#2111

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -1,19 +1,22 @@
-const avoidEmptyCity = function(a) {
-    return Object.assign({}, a, { city: a.city || '-'});
-}
+const avoidEmptyCity = function (a) {
+  return Object.assign({}, a, { city: a.city || "-" });
+};
 
 class AirportsService {
-    constructor(airports){
-        this._airports = airports;
-    }
+  constructor(airports) {
+    this._airports = airports;
+  }
 
-    getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
-    }
+  getAll() {
+    return this._airports
+      .filter((a) => a.code)
+      .map(avoidEmptyCity)
+      .sort((a, b) => (a.city > b.city ? 1 : -1));
+  }
 
-    getByCode(code) {
-        return avoidEmptyCity(this._airports.find(a => a.code == code));
-    }
+  getByCode(code) {
+    return avoidEmptyCity(this._airports.find((a) => a.code == code));
+  }
 }
 
 module.exports = AirportsService;
